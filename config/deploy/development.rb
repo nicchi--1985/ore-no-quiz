15bc47edc786c8e54beff71774ff9fa2908c26f6
nicchi--1985 <nicchi.achan@gmail.com>
2017-01-31 00:14:25 +0900

    enable specifying branch name when cap deploy command

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -4,7 +4,7 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 set :stage, :development
-set :branch, 'development'
+set :branch, fetch(:branch, 'development')
 
 role :app, %w{nicchi@160.16.232.12:50022}
 role :web, %w{nicchi@160.16.232.12}
