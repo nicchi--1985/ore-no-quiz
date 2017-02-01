9f98e3ead755046612b8f9a871b4a03d9450d8c2
nicchi--1985 <nicchi.achan@gmail.com>
2017-02-02 01:15:33 +0900

    add quiz controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'quizzes#index'
+  get 'quizzes/show/:id' => 'quizzes#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
